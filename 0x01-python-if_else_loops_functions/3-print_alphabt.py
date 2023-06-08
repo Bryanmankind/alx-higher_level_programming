b784ce2c9c908ef0396e53b604730be41e5e8c77
Oluwasegun makinde <shegeemankind@gmail.com>
2023-06-08 14:02:28 +0000

    python functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+#3-print_alphabt.py
+
 for letter in range(97, 123):
-    if letter not in (113, 101)
+    if letter not in (113, 101):
         print("{}".format(chr(letter)), end="")
